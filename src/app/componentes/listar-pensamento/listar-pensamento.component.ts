3fe039ef95c53fc8499e1e677f3fddb0c86b8e5a
hugofsd <hugo619@outlook.com>
2025-04-15 15:46:49 -0300

    Diretivas

--- src/app/componentes/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/listar-pensamento/listar-pensamento.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo:
+        'Num futuro onde sonhos são gravados, uma técnica de cinema descobre um arquivo corrompido que mostra o assassinato de alguém que ainda está vivo. Ela tem 48 horas para provar a conspiração antes que suas próprias memórias sejam editadas e a realidade reescrita',
+      autoria: '',
+      modelo: 'modelo1',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
